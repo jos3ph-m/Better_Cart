a263d0168f2a78275c6f6e85b3bf1368f90d218d
Joseph M <jmedina1985@hotmail.com>
2023-01-10 21:21:18 -0800

    Getting started

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/Navbar';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // localhost:3000 -> Home
 // localhost:3000/success -> Success
@@ -11,6 +12,9 @@ function App() {
   return (
     <Container>
       <NavbarComponent></NavbarComponent>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
     </Container>
   );
 }
