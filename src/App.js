d29bbb475351d6240f0c060ec919d7eb949aee10
Joseph M <jmedina1985@hotmail.com>
2023-01-10 21:49:18 -0800

    creating pages for router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/Navbar';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cancel from './pages/Cancel';
+import Success from './pages/Success';
+import Store from './pages/Store';
 
 // localhost:3000 -> Home
 // localhost:3000/success -> Success
