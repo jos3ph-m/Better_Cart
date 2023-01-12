0e0bf2e683182f74b472df526eeabeaf8dcdc659
Joseph M <jmedina1985@hotmail.com>
2023-01-11 21:20:37 -0800

    Making the product store items

--- src/productsStore.js
+++ src/productsStore.js
@@ -16,4 +16,14 @@ const productsArray = [
   },
 ];
 
+function getProductData(id) {
+  let productData = productsArray.find((product) => product.id === id);
+  if (productData == undefined) {
+    console.log('Product data does not exist for ID: ' + id);
+    return undefined;
+  }
+
+  return productData;
+}
+
 export { productsArray };
