aec61ed8b4542714adc0bfd2b6bec1f5325c691d
Joseph M <jmedina1985@hotmail.com>
2023-01-11 21:13:59 -0800

    Making the product store items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Button, Container, Navbar, Modal } from 'react-bootstrap';
 function NavbarComponent() {
   return (
     <Navbar expand="sm">
-      <Navbar.Brand href="/">Better Cart</Navbar.Brand>
+      <Navbar.Brand href="/">Ecommerce store</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Button>Cart 0 Items</Button>
---
+++ src/productsStore.js
@@ -0,0 +1,17 @@
+const productsArray = [
+  {
+    id: 1,
+    name: 'Coffee',
+    price: 4.99,
+  },
+  {
+    id: 1,
+    name: 'Coffee',
+    price: 4.99,
+  },
+  {
+    id: 1,
+    name: 'Coffee',
+    price: 4.99,
+  },
+];
