1982b00cbfcc9a3b59f9f1516152d742df334dc7
Joseph M <jmedina1985@hotmail.com>
2023-01-11 21:24:41 -0800

    Making the product store items

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -4,11 +4,11 @@ import { productsArray } from '../productsStore';
 function Store() {
   return (
     <>
-      <h1>Welcome to the store!</h1>
+      <h1 align="center">Welcome to the store!</h1>
       <Row xs={1} md={3} className="g-4">
         {productsArray.map(product, index) => (
-          <Col align="center">
-            <h1>${product.name}</h1>
+          <Col align="center" key={index}>
+            <h1>{product.name}</h1>
           </Col>
         )}
         
