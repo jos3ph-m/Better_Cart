531ed4361f281883a5bd8fa93211ae935e528295
Joseph M <jmedina1985@hotmail.com>
2023-01-12 19:23:49 -0800

    Product card

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -9,7 +9,7 @@ function Store() {
       <Row xs={1} md={3} className="g-4">
         {productsArray.map(product, index) => (
           <Col align="center" key={index}>
-            <ProductCard />
+            <ProductCard product={product}/>
           </Col>
         )}
         
