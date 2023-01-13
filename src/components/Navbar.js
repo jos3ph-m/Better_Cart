8d0958cb9f28198590e905d450ec35d91822f2ef
Joseph M <jmedina1985@hotmail.com>
2023-01-12 19:41:34 -0800

    Creating a modal for the cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,8 @@ import { useState } from 'react';
 function NavbarComponent() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <Navbar expand="sm">
