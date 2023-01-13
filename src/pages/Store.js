48fb1ce0bd57803ceb4187584f437328d059982e
Joseph M <jmedina1985@hotmail.com>
2023-01-12 19:24:56 -0800

    Product card

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -5,14 +5,15 @@ import ProductCard from '../components/ProductCard';
 function Store() {
   return (
     <>
-      <h1 align="center" className="p-3">Welcome to the store!</h1>
+      <h1 align="center" className="p-3">
+        Welcome to the store!
+      </h1>
       <Row xs={1} md={3} className="g-4">
-        {productsArray.map(product, index) => (
+        {productsArray.map((product, index) => (
           <Col align="center" key={index}>
-            <ProductCard product={product}/>
+            <ProductCard product={product} />
           </Col>
-        )}
-        
+        ))}
       </Row>
     </>
   );
