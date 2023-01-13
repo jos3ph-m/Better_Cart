6ce8d4cd968ec9fc5742abf8f6fba8b74c509c24
Joseph M <jmedina1985@hotmail.com>
2023-01-13 11:21:54 -0800

    productsStore update

--- src/productsStore.js
+++ src/productsStore.js
@@ -20,7 +20,6 @@ function getProductData(id) {
   let productData = productsArray.find((product) => product.id === id);
   if (productData == undefined) {
     console.log('Product data does not exist for ID: ' + id);
-    return undefined;
   }
 
   return productData;
