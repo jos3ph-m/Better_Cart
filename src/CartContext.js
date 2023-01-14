c3857587f8e1dbdbabd672104afae2ef337348f2
Joseph M <jmedina1985@hotmail.com>
2023-01-13 22:33:20 -0800

    Working on cart context

--- src/CartContext.js
+++ src/CartContext.js
@@ -2,3 +2,6 @@ import { createContext, useState } from 'react';
 import { productsArray } from './productsStore';
 
 const CartContext = createContext({});
+
+// Context (cart, addToCart, removeCart)
+// Provider -> gives your React app access to all the things in your context
