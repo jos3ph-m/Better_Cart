77294de2bd17472862aa618a173848c5b18ad432
Joseph M <jmedina1985@hotmail.com>
2023-01-13 22:36:05 -0800

    Working on cart context

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,7 +1,7 @@
 import { createContext, useState } from 'react';
 import { productsArray } from './productsStore';
 
-const CartContext = createContext({
+export const CartContext = createContext({
   items: [],
   getProductQuantity: () => {},
   addOneToCart: () => {},
