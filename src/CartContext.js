4bf61c5a28539380c25757e6b9a9dc2e34395be4
Joseph M <jmedina1985@hotmail.com>
2023-01-14 21:39:21 -0800

    Working on cartContext

--- src/CartContext.js
+++ src/CartContext.js
@@ -16,6 +16,8 @@ export function CartProvider({ children }) {
   // in our cart we will store {id, quantity}
   // for instance [ { id: 1, quantity: 2}]
 
+  function getProductQuantity(id) {}
+
   const contextValue = {
     items: cartProducts,
     getProductQuantity,
