adaccaddf95ddca6a3679f7dba870b97a6c1de13
Joseph M <jmedina1985@hotmail.com>
2023-01-16 21:37:13 -0800

    Working on CartContext

--- src/CartContext.js
+++ src/CartContext.js
@@ -46,6 +46,8 @@ export function CartProvider({ children }) {
   }
 
   function deleteFromCart(id) {
+    // [] if an object meets a condition, add the object to an array
+    // [product1, product2, product3]
     setCartProducts((cartProducts) => cartProducts.filter());
   }
 
