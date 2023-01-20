6d7fd69bf234b9b2e093e956feefefb8c8e1b734
Joseph M <jmedina1985@hotmail.com>
2023-01-19 19:56:59 -0800

    Showing cart data in modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ function NavbarComponent() {
                 <h1>{currentProduct.title}</h1>
               ))}
               <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
-              <Button></Button>
+              <Button variant="success"></Button>
             </>
           ) : (
             <h1>There are no items in your cart</h1>
