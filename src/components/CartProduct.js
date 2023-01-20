68600f7aede9724e60558792ec2df0a03b216855
Joseph M <jmedina1985@hotmail.com>
2023-01-20 13:03:49 -0800

    CartProduct component

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -5,6 +5,8 @@ import { getProductData } from '../productsStore';
 
 function CartProduct(props) {
   const cart = useContext(CartContext);
+  const id = props.id;
+  const quantity = props.quantity;
 }
 
 export default CartProduct;
