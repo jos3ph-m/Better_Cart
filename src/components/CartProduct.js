0805b17ec0e5896d0550997d74ed1ebfd10fa74b
Joseph M <jmedina1985@hotmail.com>
2023-01-20 12:59:03 -0800

    CartProduct component

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -3,6 +3,8 @@ import { CartContext } from '../CartContext';
 import { useContext } from 'react';
 import { getProductData } from '../productsStore';
 
-function CartProduct(props) {}
+function CartProduct(props) {
+  const cart = useContext(CartContext);
+}
 
 export default CartProduct;
