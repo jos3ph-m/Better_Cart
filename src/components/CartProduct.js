8d3bee2f6bc20b2dd9473efc16296f1d96e0fcab
Joseph M <jmedina1985@hotmail.com>
2023-01-20 13:07:46 -0800

    CartProduct component

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -14,6 +14,7 @@ function CartProduct(props) {
       <h3>{productData.title}</h3>
       <p>{quantity} total</p>
       <p>${(quantity * productData.price).toFixed(2)}</p>
+      <Button size="sm">Remove</Button>
     </>
   );
 }
