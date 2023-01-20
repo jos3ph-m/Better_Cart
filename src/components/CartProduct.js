8fd55bdf72392cd5a024337a21b07c85670004cb
Joseph M <jmedina1985@hotmail.com>
2023-01-20 12:58:47 -0800

    CartProduct component

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,6 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import { CartContext } from '../CartContext';
 import { useContext } from 'react';
+import { getProductData } from '../productsStore';
 
 function CartProduct(props) {}
 
