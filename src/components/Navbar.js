fc6f246a454701e0bd5832cfea3371375f366e43
Joseph M <jmedina1985@hotmail.com>
2023-01-21 21:02:33 -0800

    CartProduct component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,11 +36,11 @@ function NavbarComponent() {
             <>
               <p>Items in your cart:</p>
               {cart.items.map((currentProduct, index) => (
-                <CardProduct
+                <CartProduct
                   key={index}
                   id={currentProduct.id}
                   quantity={currentProduct.quantity}
-                ></CardProduct>
+                ></CartProduct>
               ))}
               <h1>Total: ${cart.getTotalCost().toFixed(2)}</h1>
               <Button variant="success">Purchase items</Button>
