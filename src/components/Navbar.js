afa13faeecf6673e9647b0217fd715612c78a476
Joseph M <jmedina1985@hotmail.com>
2023-01-23 15:11:32 -0800

    creating checkout call

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function NavbarComponent() {
   const handleShow = () => setShow(true);
 
   const checkout = async () => {
-    await fetch();
+    await fetch('http://localhost:4000/checkout');
   };
 
   const productsCount = cart.items.reduce(
